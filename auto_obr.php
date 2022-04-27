0d8ef4139536c40963f4fc8199492527e818bcbf
YuriyGindich <103996461+YuriyGindich@users.noreply.github.com>
2022-04-27 13:49:46 +0300

    Update auto_obr.php

--- auto_obr.php
+++ auto_obr.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 header('Content-Type: text/html; charset=utf-8');
 
 $mysqli = mysqli_connect("localhost", "peablcyy_1207", "concept3", "peablcyy_1207");
@@ -7,19 +8,32 @@ if ($mysqli == false) {
   // print("error " . mysqli_connect_error());
   print("error");
 } else {
+  $email = trim(mb_strtolower($_POST['email']));
+  $pass = trim($_POST['pass']);
 
-$email = $_POST['email'];
-$pass = $_POST['pass'];
+  $result = $mysqli->query("SELECT * FROM `users` WHERE `email` = '$email'");
 
-$result = $mysqli->query("SELECT * FROM `users` WHERE `email` = '$email' AND `pass` = '$pass'");
+  $result = $result->fetch_assoc();
 
-if ($result->num_rows !== 0) {
-  print("exist");
-} else {
-  $mysqli->query("INSERT INTO `user`(`email`, `pass`) VALUES ('$email', '$pass')");
-  print("ok");
+  if (password_verify($pass, $result['pass'])) {
+    echo "ok";
+    $_SESSION['name'] = $result['name'];
+    $_SESSION['lastname'] = $result['lastname'];
+    $_SESSION['email'] = $result['email'];
+    $_SESSION['id'] = $result['id'];
+  } else {
+    echo "user_not_found";
+  }
 }
-}
+
+// if ($result->num_rows == 0) {
+//   // print("exist");
+
+// } else {
+//   // $mysqli->query("INSERT INTO `user`(`email`, `pass`) VALUES ('$email', '$pass')");
+//   // print("ok");
+
+// }
 
 
 // echo "Email: $email<br>Пароль: $pass<br>";
